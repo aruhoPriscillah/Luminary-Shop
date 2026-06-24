8c1e46497cdf1b62a68644fa515dbef6cf0e51cd
aruhoPriscillah <priscaaruho@gmail.com>
2026-06-24 10:52:35 +0300

    Fix site domain for Render

--- build.sh
+++ build.sh
@@ -3,3 +3,11 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py seed_data
+python manage.py shell -c "
+from django.contrib.sites.models import Site
+site, created = Site.objects.get_or_create(id=1)
+site.domain = 'luminary-shop.onrender.com'
+site.name = 'luminary-shop.onrender.com'
+site.save()
+print('Site updated!')
+"
