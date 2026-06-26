d7886a469fb7881dd76fabbf26a277a8a0fe9c00
aruhoPriscillah <priscaaruho@gmail.com>
2026-06-26 11:28:13 +0300

    Fix storage in settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -41,7 +41,14 @@ CLOUDINARY_STORAGE = {
     'PREFIX': '',
 }
 
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+STORAGES = {
+    'default': {
+        'BACKEND': 'cloudinary_storage.storage.MediaCloudinaryStorage',
+    },
+    'staticfiles': {
+        'BACKEND': 'django.contrib.staticfiles.storage.StaticFilesStorage',
+    },
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
