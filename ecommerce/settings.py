c23f7d0e07bddc9a72b0d4740fb036986f9e68be
aruhoPriscillah <priscaaruho@gmail.com>
2026-06-26 10:44:40 +0300

    changed media in settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -118,7 +118,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
